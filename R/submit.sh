91badd74a51f809d64a0b46eaed6f9b595b58c6f
dali <dlguo@purdue.edu>
2018-06-12 14:35:23 -0400

    put cutoff and winsize into args

--- R/submit.sh
+++ R/submit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 SESS=$1
+WINSIZE=$2
+CUTOFF=$3
 for i in $(ls $DATA/results_SIFT2/); do
-  qsub -q standby -v SUBJ=${i},SESS=${SESS} get_nets.sub
+  qsub -q standby -v SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} get_nets.sub
 done
