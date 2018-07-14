cb1aa4590e9a520976a25d9bdd4013c692310b35
dlguo <daleyguo@gmail.com>
2018-07-14 17:05:34 -0400

    init bash works with arg

--- R/submit.sh
+++ R/submit.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# SESS=$1
-# WINSIZE=$2
-# CUTOFF=$3
-# for i in $(ls $DATA/results_SIFT2/); do
-#   qsub -q standby -v SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} get_nets.sub
-# done
-
-SESS=$1
-WINSIZE=$2
-for i in $(ls $DATA/results_SIFT2/); do
-  qsub -q standby -v SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE} get_mod_trans.sub
-done
+if [ "$1" == "gen" ]; then
+  SESS=$2
+  WINSIZE=$3
+  CUTOFF=$4
+  for i in $(ls $DATA/results_SIFT2/); do
+    qsub -q standby -v SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} get_nets.sub
+  done
+elif [ "$1" == "mod" ]; then
+  SESS=$2
+  WINSIZE=$3
+  for i in $(ls $DATA/results_SIFT2/); do
+    qsub -q standby -v SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE} get_mod_trans.sub
+  done
+fi
