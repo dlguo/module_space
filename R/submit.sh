2d9304113167f1615a860a5859e31b634f97cd06
dali <dlguo@purdue.edu>
2018-08-13 15:26:43 -0400

    single run of dtw

--- R/submit.sh
+++ R/submit.sh
@@ -4,6 +4,10 @@ FUNC=$1
 SESS=$2
 WINSIZE=$3
 CUTOFF=$4
-for i in $(ls $DATA/results_SIFT2/); do
-  qsub -q standby -v FUNC=${FUNC},SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} main.sub
-done
+if [ ${FUNC} = "dtw" ]; then
+  qsub -q standby -v FUNC=${FUNC},SUBJ="NA",SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} main.sub
+else
+  for i in $(ls $DATA/results_SIFT2/); do
+    qsub -q standby -v FUNC=${FUNC},SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} main.sub
+  done
+fi
