98ab8cf66156df178e724000fe63880a3a089baf
dali <dlguo@purdue.edu>
2018-08-13 14:40:25 -0400

    fix arg parse bug

--- R/submit.sh
+++ R/submit.sh
@@ -5,5 +5,5 @@ SESS=$2
 WINSIZE=$3
 CUTOFF=$4
 for i in $(ls $DATA/results_SIFT2/); do
-  qsub -q standby -v FUNC=${FUNC}, SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} main.sub
+  qsub -q standby -v FUNC=${FUNC},SUBJ=${i},SESS=${SESS},WINSIZE=${WINSIZE},CUTOFF=${CUTOFF} main.sub
 done
