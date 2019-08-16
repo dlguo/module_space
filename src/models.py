f37170ddeb2728f7bed00d489e46ddb591d64d59
dlguo <daleyguo@gmail.com>
2019-08-16 13:52:20 -0400

    combine loss function

--- src/models.py
+++ src/models.py
@@ -74,13 +74,15 @@ class VAE_bin(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-def loss_function(recon_x, x, mu, logvar):
+def bin_loss_function(recon_x, x, mu, logvar):
+    #BCE = F.binary_cross_entropy(recon_x, x.view(-1, 64620), reduction='sum')
     BCE = F.mse_loss(recon_x, x.view(-1, 64620), reduction='sum')
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    #print("BCE: %f, KLD: %f \n" % (BCE, KLD))
 
     return BCE + KLD
 
